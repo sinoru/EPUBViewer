21ffb75b8ee1034811b79fc0b9dafa5992c5e518
Jaehong Kang <j.kang@ridi.com>
2020-01-15 17:20:13 +0900

    Add WebViewController

---
+++ EPUBViewer/EPUBViewer/WebViewController.swift
@@ -0,0 +1,33 @@
+//
+//  WebViewController.swift
+//  EPUBViewer
+//
+//  Created by Jaehong Kang on 2020/01/15.
+//  Copyright © 2020 Jaehong Kang. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController {
+    lazy var webView: WKWebView
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
