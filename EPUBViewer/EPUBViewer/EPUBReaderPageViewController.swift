b4bf53505094e20a3736fcfdf583ec17685300d8
Jaehong Kang <j.kang@ridi.com>
2020-01-15 16:33:34 +0900

    Implementing EPUBReaderPageViewController

--- EPUBViewer/EPUBViewer/EPUBReaderPageViewController.swift
+++ EPUBViewer/EPUBViewer/EPUBReaderPageViewController.swift
@@ -11,41 +11,63 @@ import EPUBKit
 import SwiftUI
 
 class EPUBReaderPageViewController: UIViewController {
+    static let pageBufferSize = 1
     typealias WebViewController = UIHostingController<WebView>
 
     var epub: EPUB?
 
     private var pageViewController: _PageViewController?
 
-    private var webViewControllers = [WebViewController]()
+    private var previousWebViewControllers = [WebViewController]()
+    private var nextWebViewControllers = [WebViewController]()
+
+    func updateWebViewControllers(reusableWebViewControllers: [WebViewController] = []) {
+        let currentViewController = pageViewController?.viewControllers ?? []
+        let isDoubleSided = pageViewController?.isDoubleSided ?? true
+
+        let pageBufferSize = (isDoubleSided ? Self.pageBufferSize * 2 : Self.pageBufferSize)
+
+        var reusableWebViewControllers = reusableWebViewControllers
+
+        previousWebViewControllers.removeAll(where: { currentViewController.contains($0) })
+        nextWebViewControllers.removeAll(where: { currentViewController.contains($0) })
+
+        if previousWebViewControllers.count < pageBufferSize {
+            (1...(pageBufferSize - previousWebViewControllers.count)).forEach { (_) in
+
+            }
+        }
+
+
+
+        if nextWebViewControllers.count < pageBufferSize {
+
+        }
+    }
 
 }
 
 extension EPUBReaderPageViewController: UIPageViewControllerDelegate {
     func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController]) {
-
+        
     }
 
     func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        guard let previousViewControllers = previousViewControllers as? [WebViewController] else {
+            fatalError("previousViewControllers should be \([WebViewController].self)")
+        }
 
+        updateWebViewControllers(reusableWebViewControllers: previousViewControllers)
     }
 }
 
 extension EPUBReaderPageViewController: UIPageViewControllerDataSource {
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        guard let webViewController = viewController as? WebViewController else {
-            fatalError("viewController should be \(WebViewController.self) not \(type(of: viewController).self)")
-        }
-
-        return webViewControllers.firstIndex(of: webViewController).flatMap { self.webViewControllers[$0 < self.webViewControllers.count - 1 ? $0 + 1 : 0] }
+        return previousWebViewControllers.last
     }
 
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        guard let webViewController = viewController as? WebViewController else {
-            fatalError("viewController should be \(WebViewController.self) not \(type(of: viewController).self)")
-        }
-
-        return webViewControllers.firstIndex(of: webViewController).flatMap { self.webViewControllers[$0 > 0 ? $0 - 1 : self.webViewControllers.endIndex - 1 ] }
+        return nextWebViewControllers.first
     }
 }
 
